a4bcc5bd919fa59d3d7acddbe31217578d750e93
Yan Turnt <bx6xbchosen1@gmail.com>
2024-12-13 19:49:20 +0500

    feat: add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
+import path from 'path'
+
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, 'src'),
+    },
+  },
   server: {
     open: true,
   },
