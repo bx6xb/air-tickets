01116307b0dbdf07e8a14d08058cc7d6e02e3bff
Yan Turnt <bx6xbchosen1@gmail.com>
2024-12-15 12:06:47 +0500

    feat: tried to add ie11 support

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,8 @@
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
+
+/* eslint-disable sort-imports */
+
 import { createRoot } from 'react-dom/client'
 
 import '@fontsource/roboto'
